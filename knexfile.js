018d31ed4ee13a67bddc623fa31e484d2317ffce
Emma Booker <RileyIO@users.noreply.github.com>
2018-03-09 22:40:17 -0400

    Fixing db decision always defaulting to Sqlite

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
+require("dotenv").config();
 
-module.exports = {
-  /** 
-   * mysql2 
+const MYSQL = {
+  /**
+   * mysql2
    **/
-  client: 'mysql2',
+  client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
@@ -17,29 +17,43 @@ module.exports = {
     max: 10
   },
   migrations: {
-    directory: './migrations/',
-    tableName: 'knex_migrations'
+    directory: "./migrations/",
+    tableName: "knex_migrations"
   },
   seeds: {
-    directory: './db/seeds/'
-  },
-  /** 
-   * sqlite3 
+    directory: "./db/seeds/"
+  }
+};
+
+const SQLITE = {
+  /**
+   * sqlite3
    **/
-  client: 'sqlite3',
+  client: "sqlite3",
   connection: {
     filename: `${process.env.DB_NAME}.sqlite`
   },
-  pool: {
-    min: 2,
-    max: 10
-  },
   migrations: {
-    directory: './migrations/',
-    tableName: 'knex_migrations'
+    directory: "./migrations/",
+    tableName: "knex_migrations"
   },
   seeds: {
-    directory: './db/seeds/'
+    directory: "./db/seeds/"
   },
   useNullAsDefault: true
+};
+
+/**
+ * Determine on file call which databse to return
+ */
+console.log(`Checking DB type defined = ${process.env.DB_TYPE}`)
+
+switch (process.env.DB_TYPE.toLocaleLowerCase()) {
+  case 'mysql2':
+    module.exports = MYSQL;
+    break;
+
+  default:
+    module.exports = SQLITE;
+    break;
 }
