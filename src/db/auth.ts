bcb784965e74477151fc9f28968eeb52e1faf012
Emma Booker <RileyIO@users.noreply.github.com>
2018-03-09 23:14:10 -0400

    Fixing test never exiting on newer mocha test releases

--- src/db/auth.ts
+++ src/db/auth.ts
@@ -1,24 +1,29 @@
 import Database from '../db';
-const scrypt = require('scrypt')
+const scrypt = require('scrypt');
 
 export default class AuthDB extends Database {
   constructor() {
-    super()
+    super();
   }
 
   /**
    * Add authentication key for application
-   * 
+   *
    * @param {number} appid
    * @param {string} key random & hashed (hash stored)
-   * @param {number} expires in number of days
+   * @param {number} expires in number of daysƒ
    * @memberof AuthDB
    */
   public async add(appid: number, key: string, expires: number): Promise<any> {
-    let expiryDate = new Date(new Date().setDate(new Date().getDate() + expires))
-    await this.db
+    let expiryDate = new Date(
+      new Date().setDate(new Date().getDate() + expires)
+    );
+
+    // Insert into db
+    return await this.db
       .table(`auth`)
       .insert({ appid: appid, key: key, expires: expiryDate })
+      .then((result) => { return true });
   }
 
   public async validate(appuuid: string, key: string): Promise<boolean> {
@@ -27,36 +32,40 @@ export default class AuthDB extends Database {
       .table(`auth`)
       .select('*')
       .where('applications.uuid', '=', appuuid)
-      .andWhere('auth.enabled', '=', true)
+      .andWhere('auth.enabled', '=', true);
 
     // console.log(appAuths, appAuths.length)
 
     // If there are no results, return false to valid
-    if (appAuths.length === 0) return false
+    if (appAuths.length === 0) return false;
 
     // Verify key against salted hash
-    if (appAuths.length > 1) return this.validateMultiple(appAuths, key)
+    if (appAuths.length > 1) return this.validateMultiple(appAuths, key);
     // Verify key against salted hash (single)
-    if (appAuths.length === 1) return this.validateSingle(appAuths[0], key)
+    if (appAuths.length === 1) return this.validateSingle(appAuths[0], key);
   }
 
   private validateMultiple(array: Array<any>, key: string): boolean {
-    console.log(`Auth->Validate found multiple [${array.length}], checking each`)
+    console.log(
+      `Auth->Validate found multiple [${array.length}], checking each`
+    );
 
     for (var index = 0; index < array.length; index++) {
       var app = array[index];
-      if (this.validateSingle(app, key, index)) return true
+      if (this.validateSingle(app, key, index)) return true;
     }
 
     // Fallback, none valid, fail
-    return false
+    return false;
   }
 
   private validateSingle(appAuth: any, key: string, index?: number): boolean {
     let valid = scrypt.verifyKdfSync(
-      new Buffer(appAuth.key, 'base64'), new Buffer(key))
+      new Buffer(appAuth.key, 'base64'),
+      new Buffer(key)
+    );
 
-    console.log(`Auth->Validating (${index || 0}) === ${valid}`)
-    return valid
+    // console.log(`Auth->Validating (${index || 0}) === ${valid}`)
+    return valid;
   }
 }
--- tests/test.js
+++ tests/test.js
@@ -1,51 +1,57 @@
 /* globals describe, before, it */
 const hasha = require('hasha');
-const should = require('should')
+const should = require('should');
 const v4 = require('uuid/v4');
-const scrypt = require('scrypt')
+const scrypt = require('scrypt');
 // ESS Modules & Classes
-const DotEnv = require('../app/dotenv')
-const ApplicationDB = require('../app/db/application')
-const AuthDB = require('../app/db/auth')
+const DotEnv = require('../app/dotenv');
+const ApplicationDB = require('../app/db/application');
+const AuthDB = require('../app/db/auth');
 
 describe('electron-stats-server (ESS) tests', () => {
   // Start ESS server
   // require('../app/app')
-  var appDB
-  var appuuid = v4()
-  var appid = undefined
-  var appInfo = {}
-  var authPrivateKey = undefined
+  var appDB;
+  var appuuid = v4();
+  var appid = undefined;
+  var appInfo = {};
+  var authPrivateKey = undefined;
 
-  /*-----------------------------*
+  /* -----------------------------*
    * Class: DotEnv
    *-----------------------------*/
   describe('.env ( Class:DotEnv )', () => {
-    const denv = new DotEnv.default()
+    const denv = new DotEnv.default();
 
-    it('dotEnv: string - .env File Path', () => { denv.dotEnv });
-    it('dotEnvBak: string - .env.bak File Path', () => { denv.dotEnvBak });
-    it('dotEnvTemplate: string - .envTemplate Path', () => { denv.dotEnvTemplate });
+    it('dotEnv: string - .env File Path', () => {
+      denv.dotEnv;
+    });
+    it('dotEnvBak: string - .env.bak File Path', () => {
+      denv.dotEnvBak;
+    });
+    it('dotEnvTemplate: string - .envTemplate Path', () => {
+      denv.dotEnvTemplate;
+    });
 
     it('doesFileExist(): boolean', () => {
-      should(denv.doesFileExist(denv.dotEnv)).be.Boolean()
+      should(denv.doesFileExist(denv.dotEnv)).be.Boolean();
     });
     it('generateDotEnv(): void', () => {
-      let envExists = denv.doesFileExist(denv.dotEnv)
+      let envExists = denv.doesFileExist(denv.dotEnv);
       // Just test that function exists
-      should(denv.generateDotEnv).be.Function()
+      should(denv.generateDotEnv).be.Function();
     });
     it('updateFile(): void', () => {
       // Just test that function exists
-      should(denv.updateFile).be.Function()
+      should(denv.updateFile).be.Function();
     });
     it('readFile(): string', () => {
       // Just test that function exists
-      should(denv.readFile).be.Function()
+      should(denv.readFile).be.Function();
     });
     it('writeFile(): void', () => {
       // Just test that function exists
-      should(denv.writeFile).be.Function()
+      should(denv.writeFile).be.Function();
     });
   });
 
@@ -53,15 +59,15 @@ describe('electron-stats-server (ESS) tests', () => {
    * Class: ApplicationDB
    *-----------------------------*/
   describe('db => Application ( Class:ApplicationDB )', () => {
-    const appDB = new ApplicationDB.default()
+    const appDB = new ApplicationDB.default();
 
     it('async add() => Add Application to DB', async () => {
-      appid = await appDB.add('Test App', appuuid)
-      should(appid).be.a.Number()
+      appid = await appDB.add('Test App', appuuid);
+      should(appid).be.a.Number();
     });
     it('async get() => Get added Application from DB', async () => {
-      appInfo = await appDB.get(appuuid, 1)
-      should(appInfo).be.an.Object()
+      appInfo = await appDB.get(appuuid, 1);
+      should(appInfo).be.an.Object();
     });
   });
 
@@ -69,22 +75,23 @@ describe('electron-stats-server (ESS) tests', () => {
    * Class: AuthDB
    *-----------------------------*/
   describe('db => Authentication ( Class:AuthDB )', () => {
-    const authDB = new AuthDB.default()
+    const authDB = new AuthDB.default();
 
     it('async add() => Add application authentication key', async () => {
-      authPrivateKey = hasha(v4())
-      let salted = await scrypt.kdf(new Buffer(authPrivateKey), { N: 1, r: 1, p: 1 })
+      authPrivateKey = hasha(v4());
+      let salted = await scrypt.kdf(new Buffer(authPrivateKey), {
+        N: 1,
+        r: 1,
+        p: 1
+      });
 
       // Generate new and associate
-      return await authDB.add(
-        appid,
-        salted.toString('base64'),
-        365
-      )
+      let test = await authDB.add(appid, salted.toString('base64'), 365);
+      should(test).be.True();
     });
     it('async validate() => Validate newly added auth record', async () => {
-      let valid = await authDB.validate(appuuid, authPrivateKey)
-      should(valid).be.True()
+      let valid = await authDB.validate(appuuid, authPrivateKey);
+      should(valid).be.True();
     });
   });
 });
